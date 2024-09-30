8b666aceac85d0780a6e1f0f21cf5b86b5e95d83
Spectre017 <spectre2162@gmail.com>
2024-09-30 15:32:31 +0200

    Continuer ChercheurAnomalie

--- ChercheurAnomalie.py
+++ ChercheurAnomalie.py
@@ -4,9 +4,28 @@ import numpy as np
 
 data = pd.read_csv("data/books.csv")
 
+#cols = ["Unnamed: " + str(i) for i in range(24,86)]
 str_column="Unnamed: "
 
+#print(data.shape)
+
+#Purge des lignes contenant des informations dans des colonnes inexistantes
+
 for i in range(24,86):
+    index = 0
     for y in data[str_column+str(i)]:
-        if (y != ):
-            print(y)
+        # remove rows 
+        if (pd.notna(y)):
+            data = data.drop([index])
+        index += 1
+            
+print(data.columns)
+
+data = data.drop(data["Unnamed: 24"], axis=1)
+
+#Purge des colonnes inexistantes
+
+for i in range(24,86):
+#    data = data.drop(columns=data[str_column+str(i)])
+
+
