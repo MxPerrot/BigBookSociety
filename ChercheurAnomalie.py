a8bf656a88e31f3249612110211c85b2a9c09612
Spectre017 <spectre2162@gmail.com>
2024-10-01 10:05:18 +0200

    Nettoyeur ISBN13

--- ChercheurAnomalie.py
+++ ChercheurAnomalie.py
@@ -23,6 +23,50 @@ for i in range(24,87):
 for i in range(24,87):
  data = data.drop(columns=[str_column+str(i)])
 
-print(data.columns)
-print(data.shape)
+#Détecter Anomalie colonne
 
+
+
+booksFormat={"id":int,
+"title":str,
+"series":str,
+"author":str,
+"rating_count":int,
+"review_count":int,
+"average_rating":float,
+"five_star_ratings":int,
+"four_star_ratings":int,
+"three_star_ratings":int,
+"two_star_ratings":int,
+"one_star_ratings":int,
+"number_of_pages":int,
+"date_published":str,
+"publisher":str,
+"original_title":str,
+"genre_and_votes":str,
+"isbn":str,
+"isbn13":int,
+"settings":str,
+"characters":str,
+"awards":str,
+"books_in_series":str,
+"description":str,
+}
+
+# Nettoyeur ISBN13 Complet
+
+# index = 0
+# for i in data['isbn13']:
+#     if ((type(i) == float or i.isnumeric()) and pd.notna(i)):
+#         data["isbn13"] = data["isbn13"].replace([i], int(i))
+#     elif (type(i) == int) or pd.isna(i): pass
+#     else: data = data.drop([index])
+#     index+=1
+
+index = 0
+for i in data:
+    if (i!="isbn13"):
+        for y in data[i]:
+            if (type(y)!=booksFormat[i] and (pd.notna(y))):
+                print(y,type(y),i)
+    index+=1
