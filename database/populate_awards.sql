9b56ad36e702f2888f189474d49520687a345b3a
FlorianNormand <flo1.normand@gmail.com>
2024-11-12 17:41:17 +0100

    updapte avec wbimport

--- database/populate_awards.sql
+++ database/populate_awards.sql
@@ -6,8 +6,13 @@ CREATE TEMP TABLE temp_awards (
     awardDate DATE
 );
 
-\copy temp_awards(awardName, awardDate) FROM '../data/Cleaned_books2.csv' DELIMITER ',' CSV HEADER;
-
+WbImport
+-usePgCopy
+-type=text
+-file='../data/Cleaned_books2.csv'
+-table=temp_awards
+-delimiter=','
+-header=true;
 
 INSERT INTO _prix (nom_prix, annee_prix)
 SELECT DISTINCT awardName, EXTRACT(YEAR FROM awardDate)::INT
