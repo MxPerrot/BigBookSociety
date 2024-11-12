bb32e31fb567b4970a1a9614616eb4bda3de3eac
EwanLansonneurIUTlan <ewan.lansonneur@etudiant.univ-rennes.fr>
2024-11-12 17:02:34 +0100

    Creation csv livre et correction d'autres csv

--- Setting_Separation_Livres.py
+++ Setting_Separation_Livres.py
@@ -17,7 +17,7 @@ countries = books['settingCountry'].unique()
 
 countries = countries[~pd.isnull(countries)]
 
-# Création dataframe pour la table pays
+# Création d'un dataframe pour la table pays
 dataset = pd.DataFrame({'nom': countries})
 
 dataset.index = dataset.index+1
@@ -25,7 +25,7 @@ dataset.index = dataset.index+1
 dataset = dataset.reset_index(names=['id_pays'])
 dataset.to_csv("./SQL/pays.csv", index=False)
 
-#Création dataframe pour la table setting
+#Création d'un dataframe pour la table setting
 settingData = pd.merge(books, dataset, left_on='settingCountry', right_on="nom", how='inner')
 settingData = settingData[['id','settingDate','settingLoc','id_pays']]
 settingData = settingData.drop_duplicates()
@@ -36,16 +36,26 @@ settingData = settingData.rename(columns={"id": "id_livre"})
 settingData.index = settingData.index+1
 settingData = settingData.reset_index(names=['id_cadre'])
 
-# Création dataframe pour le lien entre les livres et leur setting
+# Création d'un dataframe pour le lien entre les livres et leur setting
 settingLinkData = settingData[['id_cadre','id_livre']]
 settingLinkData = settingLinkData.drop_duplicates()
-settingLinkData.to_csv("./SQL/livre_cadre.csv", index=False)
+settingLinkData.to_csv("./SQL/cadre_livre.csv", index=False)
 
 settingData = settingData.drop(columns=['id_livre'])
 settingData.to_csv("./SQL/cadre.csv", index=False)
 
-#Création dataframe pour la table setting
-booksData = books[['id','title','rating_count','review_count','average_rating','five_star_ratings','four_star_ratings','three_star_ratings','two_star_ratings','one_star_ratings','number_of_pages','date_published','original_title','isbn','isbn13','description']]
+#Création d'un dataframe pour la table editeur
+publishers = books['publisher'].unique()
+publishers = publishers[~pd.isnull(publishers)]
+dataset = pd.DataFrame({'nom_editeur': publishers})
+dataset.index = dataset.index+1
+dataset = dataset.reset_index(names=['id_editeur'])
+dataset.to_csv("./SQL/editeur.csv", index=False)
+
+
+#Création d'un dataframe pour la table livres
+publisherLinkData = pd.merge(books, dataset, left_on='publisher', right_on="nom_editeur", how='inner')
+booksData = publisherLinkData[['id','title','rating_count','review_count','average_rating','five_star_ratings','four_star_ratings','three_star_ratings','two_star_ratings','one_star_ratings','number_of_pages','date_published','original_title','isbn','isbn13','description','id_editeur']]
 booksData = booksData.drop_duplicates()
 booksData = booksData.drop_duplicates(subset=['id'],keep='first')
 
