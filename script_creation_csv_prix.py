5410a95e5cef20d147d6a5ba60d561c06e8e3158
FlorianNormand <flo1.normand@gmail.com>
2024-11-13 10:17:56 +0100

    creation script pour générer prix.csv

---
+++ script_creation_csv_prix.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+# Charger le fichier CSV et récupérer uniquement les colonnes souhaitées
+df = pd.read_csv('data/IGNOREME_Cleaned_books2.csv', usecols=['awardDate', 'awardName'], dtype={
+    'awardDate': 'Int32'
+    })
+
+
+
+# Ajouter une colonne 'id_prix' unique pour chaque ligne
+df['id_prix'] = range(1, len(df) + 1)
+
+# Supprimer les lignes où 'awardDate' ou 'awardName' est manquant
+df = df.dropna(subset=['awardDate', 'awardName'])
+
+# Renommer les colonnes
+df = df.rename(columns={'awardDate': 'annee_prix', 'awardName': 'nom_prix'})
+
+# Réorganiser les colonnes : 'id_prix' en premier, puis 'nom_prix' et 'annee_prix'
+df = df[['id_prix', 'nom_prix', 'annee_prix']]
+
+# Enregistrer le DataFrame dans le fichier SQL/prix.csv
+df.to_csv('SQL/prix.csv', index=False)
