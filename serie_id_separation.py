ef27096b9f64b318af6a3d255c7575490150336a
nbracquart <nathanbrct@gmail.com>
2024-11-13 09:49:40 +0100

    update avec IGNOREME_Cleaned_books2.csv pour serie_id_separation.py

--- serie_id_separation.py
+++ serie_id_separation.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 # Charger le fichier CSV
-books = pd.read_csv("Big_book.csv")
+books = pd.read_csv("data/IGNOREME_Cleaned_books2.csv")
 
-series_names = books['series'].dropna().unique()
+series = books['seriesName'].dropna().unique()
 
 # Créer un DataFrame pour la table des séries avec un identifiant unique
-series_table = pd.DataFrame({'series_name': series_names})
+series_table = pd.DataFrame({'seriesName': series})
 
 # Ajouter un identifiant unique pour chaque série
 series_table.index = series_table.index + 1 
