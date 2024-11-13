7fb5e88b905b20b990f862b9fee26cda5d641e7c
FlorianNormand <flo1.normand@gmail.com>
2024-11-13 14:51:39 +0100

    correction wb import pour peuplement avec prix.csv

--- database/populate_awards.sql
+++ database/populate_awards.sql
@@ -1,20 +1,9 @@
 set schema 'sae';
-DROP table IF EXISTS temp_awards CASCADE;
-
-CREATE TEMP TABLE temp_awards (
-    awardName VARCHAR,
-    awardDate DATE
-);
 
 WbImport
 -usePgCopy
 -type=text
--file='../data/Cleaned_books2.csv'
--table=temp_awards
+-file='../SQL/prix.csv'
+-table=_prix
 -delimiter=','
 -header=true;
-
-INSERT INTO _prix (nom_prix, annee_prix)
-SELECT DISTINCT awardName, EXTRACT(YEAR FROM awardDate)::INT
-FROM temp_awards
-WHERE awardName IS NOT NULL AND awardDate IS NOT NULL;
