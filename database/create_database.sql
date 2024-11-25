aa7de432baf5eade0bc856902b5db7f0949ebcfa
Asaiah-cosson <asaiah.cosson@etudiant.univ-rennes.fr>
2024-11-25 14:15:34 +0100

    feat: crée les tables utilisateur & corrections
    - ajout de NOT NULL sur certaines tables
    - table _utilisateur
    - tables annexes à _utilisateur (_raison_achat, _motivation, etc..)

--- database/create_database.sql
+++ database/create_database.sql
@@ -48,17 +48,17 @@ CREATE TABLE _livre (
     isbn13 VARCHAR,
     description VARCHAR,
     id_editeur INTEGER REFERENCES _editeur(id_editeur)
-    -- TODO: éventuellement créer une colonne "format", afin que les nouveaux livres ajoutés puissent avoir un format afin d'enrichir la base.  
+-- TODO: éventuellement créer une colonne "format", afin que les nouveaux livres ajoutés puissent avoir un format afin d'enrichir la base.  
 );
 
 
-CREATE TABLE IF NOT EXISTS _genre (
+CREATE TABLE _genre (
     id_genre SERIAL PRIMARY KEY,
     libelle_genre VARCHAR
 );
 
 
-CREATE TABLE IF NOT EXISTS _auteur (
+CREATE TABLE _auteur (
     id_auteur SERIAL PRIMARY KEY,
     note_moyenne DECIMAL,
     nom VARCHAR UNIQUE,
@@ -71,17 +71,17 @@ CREATE TABLE IF NOT EXISTS _auteur (
 );
 
 
-CREATE TABLE IF NOT EXISTS _code_postal (
+CREATE TABLE _code_postal (
     id_code_postal SERIAL PRIMARY KEY,
-    code_postal VARCHAR UNIQUE
+    code_postal VARCHAR UNIQUE NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS _format (
+CREATE TABLE _format (
     id_format SERIAL PRIMARY KEY,
-    libelle_format VARCHAR NOT NULL
+    libelle_format VARCHAR UNIQUE NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS _utilisateur (
+CREATE TABLE _utilisateur (
     id_utilisateur SERIAL PRIMARY KEY,
     mail_utilisateur VARCHAR UNIQUE NOT NULL,
     gender VARCHAR NOT NULL,
@@ -90,41 +90,93 @@ CREATE TABLE IF NOT EXISTS _utilisateur (
     situation_familiale VARCHAR,
     frequence_lecture VARCHAR,
     vitesse_lecture INTEGER,
-    nbr_livre_lue INTEGER,
+    nb_livres_lus INTEGER,
     id_code_postal INTEGER REFERENCES _code_postal(id_code_postal)
 );
 
+CREATE TABLE _raison_achat (
+    id_raison_achat SERIAL PRIMARY KEY,
+    raison_achat VARCHAR UNIQUE NOT NULL
+);
+
+CREATE TABLE _langue (
+    id_langue SERIAL PRIMARY KEY,
+    langue VARCHAR UNIQUE NOT NULL
+);
+
+CREATE TABLE _procuration (
+    id_procuration SERIAL PRIMARY KEY,
+    methode_procuration VARCHAR UNIQUE NOT NULL
+);
+
+CREATE TABLE _motivation (
+    id_motivation SERIAL PRIMARY KEY,
+    motivation VARCHAR UNIQUE NOT NULL
+);
 
 
 -- Table de liaison _auteur_genre pour la relation "préférence" entre Auteur et Genre
-CREATE TABLE IF NOT EXISTS _auteur_genre (
+CREATE TABLE _auteur_genre (
     id_auteur INTEGER  REFERENCES _auteur(id_auteur) ON DELETE CASCADE,
     id_genre INTEGER REFERENCES _genre(id_genre) ON DELETE CASCADE,
     PRIMARY KEY (id_auteur, id_genre)
 );
 
+
+
 -- Table de liaison _format_utilisateur pour la relation "préférence" entre Utilisateur et Format
-CREATE TABLE IF NOT EXISTS _format_utilisateur (
+CREATE TABLE _format_utilisateur (
     id_format INTEGER  REFERENCES _format(id_format) ON DELETE CASCADE,
     id_utilisateur INTEGER REFERENCES _utilisateur(id_utilisateur) ON DELETE CASCADE,
     PRIMARY KEY (id_format, id_utilisateur)
 );
 
-
-
 -- Table de liaison _utilisateur_genre pour la relation "préférence" entre Utilisateur et Genre
-CREATE TABLE IF NOT EXISTS _utilisateur_genre (
+CREATE TABLE _utilisateur_genre (
     id_utilisateur INTEGER REFERENCES _utilisateur(id_utilisateur) ON DELETE CASCADE,
     id_genre INTEGER REFERENCES _genre(id_genre) ON DELETE CASCADE,
     PRIMARY KEY (id_utilisateur, id_genre)
 );
 
 -- Table _utilisateur_auteur pour la relation de "préférence" entre Utilisateur et Auteur
-CREATE TABLE IF NOT EXISTS _utilisateur_auteur (
+CREATE TABLE _utilisateur_auteur (
     id_utilisateur INTEGER REFERENCES _utilisateur(id_utilisateur) ON DELETE CASCADE,
     id_auteur INTEGER REFERENCES _auteur(id_auteur) ON DELETE CASCADE,
     PRIMARY KEY (id_utilisateur, id_auteur)
 );
+
+-- Table _livre_utilisateur pour la relation de préférences d'un utilisateur avec ses livres préférés
+CREATE TABLE _livre_utilisateur (
+  id_utilisateur INTEGER REFERENCES _utilisateur(id_utilisateur) ON DELETE CASCADE,
+  id_livre INTEGER REFERENCES _livre(id_livre) ON DELETE CASCADE,
+  PRIMARY KEY(id_utilisateur,id_livre)
+);
+
+CREATE TABLE _utilisateur_motivation (
+  id_utilisateur INTEGER REFERENCES _utilisateur(id_utilisateur) ON DELETE CASCADE,
+  id_motivation INTEGER REFERENCES _motivation(id_motivation) ON DELETE CASCADE,
+  PRIMARY KEY(id_utilisateur,id_motivation)
+);
+
+CREATE TABLE _utilisateur_procuration (
+  id_utilisateur INTEGER REFERENCES _utilisateur(id_utilisateur) ON DELETE CASCADE,
+  id_procuration INTEGER REFERENCES _procuration(id_procuration) ON DELETE CASCADE,
+  PRIMARY KEY(id_utilisateur,id_procuration)
+);
+
+CREATE TABLE _utilisateur_langue (
+  id_utilisateur INTEGER REFERENCES _utilisateur(id_utilisateur) ON DELETE CASCADE,
+  id_langue INTEGER REFERENCES _langue(id_langue) ON DELETE CASCADE,
+  PRIMARY KEY(id_utilisateur,id_langue)
+);
+
+CREATE TABLE _utilisateur_raison_achat (
+  id_utilisateur INTEGER REFERENCES _utilisateur(id_utilisateur) ON DELETE CASCADE,
+  id_raison_achat INTEGER REFERENCES _raison_achat(id_raison_achat) ON DELETE CASCADE,
+  PRIMARY KEY(id_utilisateur,id_raison_achat)
+);
+
+
 
 -- Table _prix_livre pour la relation entre un prix et le livre attribué
 CREATE TABLE _prix_livre (
@@ -138,13 +190,6 @@ CREATE TABLE _cadre_livre (
   id_cadre INTEGER REFERENCES _cadre(id_cadre) ON DELETE CASCADE,
   id_livre INTEGER REFERENCES _livre(id_livre) ON DELETE CASCADE,
   PRIMARY KEY(id_cadre,id_livre)
-);
-
--- Table _livre_prefere_utilisateur pour la relation de préférences d'un utilisateur avec ses livres préférés
-CREATE TABLE _livre_prefere_utilisateur (
-  id_utilisateur INTEGER REFERENCES _utilisateur(id_utilisateur) ON DELETE CASCADE,
-  id_livre INTEGER REFERENCES _livre(id_livre) ON DELETE CASCADE,
-  PRIMARY KEY(id_utilisateur,id_livre)
 );
 
 -- Table _auteur_livre pour la relation entre des auteur et les livre qu'ils ont écrit
@@ -297,4 +342,3 @@ WbImport
 -table=_auteur_livre
 -delimiter=','
 -header=true;
-
