3a72cf30e5a3ace190a33746d233110d8fff29af
Asaiah-cosson <asaiah.cosson@etudiant.univ-rennes.fr>
2024-11-29 17:30:27 +0100

    feat: Créé les fichiers csv à partir du formulaire
    Tous les CSV
    - dossier externes (correction future pour le rendre interne)
    - fonctions de nettoyage + import
    - pas de gestion des fichiers déjà existants (livres, auteurs)

--- scripts/etl_scripts/csv_formulaire_table_creation.py
+++ scripts/etl_scripts/csv_formulaire_table_creation.py
@@ -9,7 +9,7 @@ def main(results):
     os.makedirs(PATH_POPULATE, exist_ok=True) #Créé le dossier voulu
 
     ##### COLONNES DU CSV #####
-    mails = results['Nom d\'utilisateur']
+    mails = results['Adresse e-mail']
     sexe = results['Je suis...']
     age = results['Quel âge avez-vous ?']
     situation_pro = results['Quelle est votre situation professionnelle ? (Optionnel)']
@@ -32,9 +32,11 @@ def main(results):
     def filtrer_choix_multiples(list) :
         res = []
         for element in list : 
-            element = element.split(';')
+            element = element.split(',')
             for el in element :
-                if (el != '') :
+                if (el.strip() != '') :
+                    el = el.strip()
+                    el = el.capitalize()
                     res.append(el)
         res = set(res)
         return res
@@ -63,44 +65,65 @@ def main(results):
 
     ##### TABLES SIMPLES #####
 
+    # _genre : id_genre, libelle_genre
+    # EXISTE DEJA
+    genre_df = pd.DataFrame({'genre': list(genres_preferes_unique)})
+    genre_df.index = genre_df.index+1
+    genre_df = genre_df.reset_index(names=['id_genre'])
+    genre_df.to_csv(os.path.join(PATH_POPULATE,"genre.csv"), index=False)
+
+    # _livre : id_livre, titre
+    # EXISTE DEJA
+    livre_df = pd.DataFrame({'livre': list(livres_preferes_unique)})
+    livre_df.index = livre_df.index+1
+    livre_df = livre_df.reset_index(names=['id_livre'])
+    livre_df.to_csv(os.path.join(PATH_POPULATE,"livre.csv"), index=False)
+
+    # _auteur : id_auteur, nom
+    # EXISTE DEJA
+    auteur_df = pd.DataFrame({'auteur': list(auteurs_preferes_unique)})
+    auteur_df.index = auteur_df.index+1
+    auteur_df = auteur_df.reset_index(names=['id_auteur'])
+    auteur_df.to_csv(os.path.join(PATH_POPULATE,"auteur.csv"), index=False)
+    
     # _motivation
-    motivation = pd.DataFrame({'motivation': list(motivation_unique)})
-    motivation.index = motivation.index+1
-    motivation = motivation.reset_index(names=['id_motivation'])
-    motivation.to_csv(os.path.join(PATH_POPULATE,"motivation.csv"), index=False)
+    motivation_df = pd.DataFrame({'motivation': list(motivation_unique)})
+    motivation_df.index = motivation_df.index+1
+    motivation_df = motivation_df.reset_index(names=['id_motivation'])
+    motivation_df.to_csv(os.path.join(PATH_POPULATE,"motivation.csv"), index=False)
 
     # _code_postal
-    code_postal = pd.DataFrame({'code_postal': list(code_postal_unique)})
-    code_postal.index = code_postal.index+1
-    code_postal = code_postal.reset_index(names=['id_code_postal'])
-    code_postal.to_csv(os.path.join(PATH_POPULATE,"code_postal.csv"), index=False)
+    code_postal_df = pd.DataFrame({'code_postal': list(code_postal_unique)})
+    code_postal_df.index = code_postal_df.index+1
+    code_postal_df = code_postal_df.reset_index(names=['id_code_postal'])
+    code_postal_df.to_csv(os.path.join(PATH_POPULATE,"code_postal.csv"), index=False)
 
     # _procuration
-    procuration = pd.DataFrame({'procuration': list(methode_procuration_unique)})
-    procuration.index = procuration.index+1
-    procuration = procuration.reset_index(names=['id_procuration'])
-    procuration.to_csv(os.path.join(PATH_POPULATE,"procuration.csv"), index=False)
+    procuration_df = pd.DataFrame({'procuration': list(methode_procuration_unique)})
+    procuration_df.index = procuration_df.index+1
+    procuration_df = procuration_df.reset_index(names=['id_procuration'])
+    procuration_df.to_csv(os.path.join(PATH_POPULATE,"procuration.csv"), index=False)
 
     # _langue
-    langue = pd.DataFrame({'langue': list(langues_unique)})
-    langue.index = langue.index+1
-    langue = langue.reset_index(names=['id_langue'])
-    langue.to_csv(os.path.join(PATH_POPULATE,"langue.csv"), index=False)
+    langue_df = pd.DataFrame({'langue': list(langues_unique)})
+    langue_df.index = langue_df.index+1
+    langue_df = langue_df.reset_index(names=['id_langue'])
+    langue_df.to_csv(os.path.join(PATH_POPULATE,"langue.csv"), index=False)
 
     # _format
-    format = pd.DataFrame({'format': list(formats_preferes_unique)})
-    format.index = format.index+1
-    format = format.reset_index(names=['id_format'])
-    format.to_csv(os.path.join(PATH_POPULATE,"format.csv"), index=False)
+    format_df = pd.DataFrame({'format': list(formats_preferes_unique)})
+    format_df.index = format_df.index+1
+    format_df = format_df.reset_index(names=['id_format'])
+    format_df.to_csv(os.path.join(PATH_POPULATE,"format.csv"), index=False)
 
     # _raison_achat
-    raison_achat = pd.DataFrame({'raison_achat': list(raison_achat_unique)})
-    raison_achat.index = raison_achat.index+1
-    raison_achat = raison_achat.reset_index(names=['id_raison_achat'])
-    raison_achat.to_csv(os.path.join(PATH_POPULATE,"raison_achat.csv"), index=False)
+    raison_achat_df = pd.DataFrame({'raison_achat': list(raison_achat_unique)})
+    raison_achat_df.index = raison_achat_df.index+1
+    raison_achat_df = raison_achat_df.reset_index(names=['id_raison_achat'])
+    raison_achat_df.to_csv(os.path.join(PATH_POPULATE,"raison_achat.csv"), index=False)
 
     # _utilisateur
-    utilisateur = pd.DataFrame({
+    utilisateur_df = pd.DataFrame({
         'mail_utilisateur': mails,
         'sexe': sexe,
         'age': age,
@@ -110,33 +133,184 @@ def main(results):
         'vitesse_lecture': vitesse,
         'nb_livres_lus': nb_livres_lus
     })
-    utilisateur.index = utilisateur.index+1
-    utilisateur = utilisateur.reset_index(names=['id_utilisateur'])
-    utilisateur.to_csv(os.path.join(PATH_POPULATE,"utilisateur.csv"), index=False)
+    utilisateur_df.index = utilisateur_df.index+1
+    utilisateur_df = utilisateur_df.reset_index(names=['id_utilisateur'])
+    utilisateur_df.to_csv(os.path.join(PATH_POPULATE,"utilisateur.csv"), index=False)
 
     ##### TABLES DE RELATION #####
+    
+    def table_relation(list, list_multiple, dic, spl=',') :
+        dic_mult = {}
+        for i in dic.values : 
+            i[1] = i[1].strip()
+            i[1] = i[1].capitalize()
+            dic_mult[i[1]] = i[0]
+
+        res = []
+        i = 0
+        for element in list_multiple :
+            id_user = i
+
+            if (isinstance(element, float) or (element == '')) :
+                if ('[non renseigné]' in dic_mult) :
+                    id_mult = dic_mult['[non renseigné]']
+                else : 
+                    id_mult = ''
+            else :
+                element = element.split(spl)
+                
+                for el in element :
+                    if (el.strip() == ''):
+                        if ('[non renseigné]' in dic_mult) :
+                            id_mult = dic_mult['[non renseigné]']
+                        else : 
+                            id_mult = ''
+                    else : 
+                        el = el.strip()
+                        el = el.capitalize()
+                        id_mult = dic_mult[el]
+                    res.append([i, id_mult])
+             
+            i += 1
+        return res
 
     # _format_utilisateur : id_format, id_utilisateur
-    #format_utilisateur = pd.merge(formats_preferes, utilisateur[''], left_on='id_format', right_on="id_utilisateur", how='inner')
-    #print(format_utilisateur)
-    #format_utilisateur = format_utilisateur[['id_format','id_utilisateur',]]
-    #format_utilisateur = format_utilisateur.drop_duplicates()
-    #format_utilisateur = format_utilisateur.rename(columns={"id_format": "id_utilisateur"})
-    #format_utilisateur.to_csv(os.path.join(PATH_POPULATE,"format_utilisateur.csv"), index=False)
+    print("/// format_utilisateur ///")
+    list_format_utilisateur = table_relation(mails,formats_preferes, format_df)
 
-    # _utilisateur_genre : id_utilisateur, id_genre
+    id_utilisateurs = []
+    id_formats = []
+    for element in list_format_utilisateur :
+        id_utilisateurs.append(element[0])
+        id_formats.append(element[1])
 
-    # _utilisateur_auteur : id_utilisateur, id_auteur
-
-    # _livre_utilisateur : id_utilisateur, id_livre
+    format_utilisateur = pd.DataFrame({
+        'id_utilisateur': id_utilisateurs,
+        'id_format': id_formats
+    })
+    format_utilisateur.index = format_utilisateur.index+1
+    format_utilisateur.to_csv(os.path.join(PATH_POPULATE,"format_utilisateur.csv"), index=False)
 
     # _utilisateur_motivation : id_utilisateur, id_motivation
+    print("/// utilisateur_motivation ///")
+    list_utilisateur_motivation = table_relation(mails, motivation, motivation_df)
 
-    # _utilisateur_procuration : _utilisateur, _procuration
+    id_utilisateurs = []
+    id_motivation = []
+    for element in list_utilisateur_motivation :
+        id_utilisateurs.append(element[0])
+        id_motivation.append(element[1])
+
+    utilisateur_motivation = pd.DataFrame({
+        'id_utilisateur': id_utilisateurs,
+        'id_motivation': id_motivation
+    })
+    utilisateur_motivation.index = utilisateur_motivation.index+1
+    utilisateur_motivation.to_csv(os.path.join(PATH_POPULATE,"utilisateur_motivation.csv"), index=False)
+
+    # _utilisateur_procuration : id_utilisateur, id_procuration
+    print("/// utilisateur_procuration ///")
+    list_utilisateur_procuration = table_relation(mails, methode_procuration, procuration_df)
+
+    id_utilisateurs = []
+    id_procuration = []
+    for element in list_utilisateur_procuration :
+        id_utilisateurs.append(element[0])
+        id_procuration.append(element[1])
+
+    utilisateur_procuration = pd.DataFrame({
+        'id_utilisateur': id_utilisateurs,
+        'id_procuration': id_procuration
+    })
+    utilisateur_procuration.index = utilisateur_procuration.index+1
+    utilisateur_procuration.to_csv(os.path.join(PATH_POPULATE,"utilisateur_procuration.csv"), index=False)
 
     # _utilisateur_langue : id_utilisateur, id_langue
+    print("/// utilisateur_langue ///")
+    list_utilisateur_langue = table_relation(mails, langues, langue_df)
+
+    id_utilisateurs = []
+    id_langue = []
+    for element in list_utilisateur_langue :
+        id_utilisateurs.append(element[0])
+        id_langue.append(element[1])
+
+    utilisateur_langue = pd.DataFrame({
+        'id_utilisateur': id_utilisateurs,
+        'id_langue': id_langue
+    })
+    utilisateur_langue.index = utilisateur_langue.index+1
+    utilisateur_langue.to_csv(os.path.join(PATH_POPULATE,"utilisateur_langue.csv"), index=False)
 
     # _utilisateur_raison_achat : id_utilisateur, id_raison_achat
+    print("/// utilisateur_raison_achat ///")
+    list_utilisateur_raison_achat = table_relation(mails, raison_achat, raison_achat_df)
+
+    id_utilisateurs = []
+    id_raison_achat = []
+    for element in list_utilisateur_raison_achat :
+        id_utilisateurs.append(element[0])
+        id_raison_achat.append(element[1])
+
+    utilisateur_raison_achat = pd.DataFrame({
+        'id_utilisateur': id_utilisateurs,
+        'id_raison_achat': id_raison_achat
+    })
+    utilisateur_raison_achat.index = utilisateur_raison_achat.index+1
+    utilisateur_raison_achat.to_csv(os.path.join(PATH_POPULATE,"utilisateur_raison_achat.csv"), index=False)
+    
+    # _utilisateur_genre : id_utilisateur, id_genre
+    print("/// utilisateur_genre ///")
+    list_utilisateur_genre = table_relation(mails, genres_preferes, genre_df)
+
+    id_utilisateurs = []
+    id_genre = []
+    for element in list_utilisateur_genre :
+        id_utilisateurs.append(element[0])
+        id_genre.append(element[1])
+
+    utilisateur_genre = pd.DataFrame({
+        'id_utilisateur': id_utilisateurs,
+        'id_genre': id_genre
+    })
+    utilisateur_genre.index = utilisateur_genre.index+1
+    utilisateur_genre.to_csv(os.path.join(PATH_POPULATE,"utilisateur_genre.csv"), index=False)
+
+    # _utilisateur_auteur : id_utilisateur, id_auteur
+    print("/// utilisateur_auteur ///")
+    
+    list_utilisateur_auteur = table_relation(mails, auteurs_preferes, auteur_df, '\n')
+
+    id_utilisateurs = []
+    id_auteur = []
+    for element in list_utilisateur_auteur :
+        id_utilisateurs.append(element[0])
+        id_auteur.append(element[1])
+
+    utilisateur_auteur = pd.DataFrame({
+        'id_utilisateur': id_utilisateurs,
+        'id_auteur': id_auteur
+    })
+    utilisateur_auteur.index = utilisateur_auteur.index+1
+    utilisateur_auteur.to_csv(os.path.join(PATH_POPULATE,"utilisateur_auteur.csv"), index=False)
+
+    # _livre_utilisateur : id_utilisateur, id_livre
+    print("/// livre_utilisateur ///")
+    
+    list_livre_utilisateur = table_relation(mails, livres_preferes, livre_df, '\n')
+
+    id_utilisateurs = []
+    id_livre = []
+    for element in list_livre_utilisateur :
+        id_utilisateurs.append(element[0])
+        id_livre.append(element[1])
+
+    utilisateur_livre = pd.DataFrame({
+        'id_utilisateur': id_utilisateurs,
+        'id_livre': id_livre
+    })
+    utilisateur_livre.index = utilisateur_livre.index+1
+    utilisateur_livre.to_csv(os.path.join(PATH_POPULATE,"livre_utilisateur.csv"), index=False)
 
 
 ##### __NAME__ #####
