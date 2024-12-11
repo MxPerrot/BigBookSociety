f35ec0de3e278f7d658b493a2ae4ff6cc60e5ccc
Spectre017 <spectre2162@gmail.com>
2024-12-11 09:58:00 +0100

    minor changes

--- recommendation_tendance.py
+++ recommendation_tendance.py
@@ -1,5 +1,16 @@
+import os
+from dotenv import load_dotenv, dotenv_values 
 import psycopg2
 
-connection = psycopg2.connect(database="pg_dgoupil", user="dgoupil", password="fqds;", host="servbdd", port=5432)
+# loading variables from .env file
+load_dotenv() 
+
+connection = psycopg2.connect(
+    database=os.getenv("DATABASE_NAME"), 
+    user=os.getenv("USERNAME"), 
+    password=os.getenv("PASSWORD"), 
+    host=os.getenv("HOST"), 
+    port=os.getenv("PORT")
+)
 
 cursor = connection.cursor()
