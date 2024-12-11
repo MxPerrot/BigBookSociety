d7a938f60ade3b8eaee054d025c25aa726d2a09e
EwanLansonneurIUTlan <ewan.lansonneur@etudiant.univ-rennes.fr>
2024-12-11 10:03:47 +0100

    Added commentary to help create .env

--- code_connec_bdd.py
+++ code_connec_bdd.py
@@ -5,6 +5,15 @@ import psycopg2
 # loading variables from .env file
 load_dotenv() 
 
+"""
+Creez un fichier .env avec la syntaxe suivante :
+    DATABASE_NAME = "pg_username"
+    USERNAME = "username"
+    PASSWORD = "password"
+    HOST = "servbdd"
+    PORT = "5432"
+"""
+
 connection = psycopg2.connect(
     database=os.getenv("DATABASE_NAME"), 
     user=os.getenv("USERNAME"), 
