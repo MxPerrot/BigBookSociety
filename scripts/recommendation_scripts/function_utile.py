bab1f96a84a70d08c728a3d364d50ea7a547e69e
Spectre017 <spectre2162@gmail.com>
2024-12-18 09:09:00 +0100

    vecteur de genre complet

--- scripts/recommendation_scripts/function_utile.py
+++ scripts/recommendation_scripts/function_utile.py
@@ -4,6 +4,11 @@ import psycopg2
 import pandas as pd
 import numpy as np
 import re
+from gensim.models import Word2Vec
+import gensim
+from nltk.tokenize import sent_tokenize, word_tokenize
+import warnings
+import itertools
 
 
 def genre_expand(genre):
@@ -11,32 +16,62 @@ def genre_expand(genre):
     res = re.split(r'[;,\s,-]+', genre)
     return res
 
-# loading variables from .env file
-load_dotenv() 
+def model_genre():
 
-connection = psycopg2.connect(
-    database=os.getenv("DATABASE_NAME"), 
-    user=os.getenv("USERNAME"), 
-    password=os.getenv("PASSWORD"), 
-    host=os.getenv("HOST"), 
-    port=os.getenv("PORT")
-)
+    # loading variables from .env file
+    load_dotenv() 
 
-cursor = connection.cursor()
+    connection = psycopg2.connect(
+        database=os.getenv("DATABASE_NAME"), 
+        user=os.getenv("USERNAME"), 
+        password=os.getenv("PASSWORD"), 
+        host=os.getenv("HOST"), 
+        port=os.getenv("PORT")
+    )
 
-cursor.execute("select id_livre,libelle_genre from sae._livre NATURAL JOIN sae._genre_livre NATURAL JOIN sae._genre;")
+    cursor = connection.cursor()
 
-record = cursor.fetchall()
+    cursor.execute("select id_livre,libelle_genre from sae._livre NATURAL JOIN sae._genre_livre NATURAL JOIN sae._genre;")
 
-for i in range(len(record)):    
-        record[i]= list(record[i])
+    record = cursor.fetchall()
 
-livre = pd.DataFrame(record, columns = ['Livre', 'Genre']) 
+    for i in range(len(record)):    
+            record[i]= list(record[i])
 
-
-livre['Genre'] = livre['Genre'].apply(genre_expand)
-
-livre = livre.explode('Genre')
+    livre = pd.DataFrame(record, columns = ['Livre', 'Genre']) 
 
 
-print(livre['Genre'].nunique())
+    livre['Genre'] = livre['Genre'].apply(genre_expand)
+
+    livre = livre.explode('Genre')
+
+    livre = livre.groupby('Livre')['Genre'].apply(list).reset_index(name='Genre')
+
+    model1 = gensim.models.Word2Vec(livre['Genre'], min_count=1,vector_size=100, window=5)
+
+    return model1
+
+
+def vect_genre(model,livre1,livre2):
+     tot = 0
+     index = 0
+     genres1 = []
+     genres2 = []
+
+     for i in livre1:
+        genres1.append(genre_expand(i))
+     for y in livre2 :
+        genres2.append(genre_expand(y))
+
+     genres1 = list(itertools.chain.from_iterable(genres1))
+     genres2 = list(itertools.chain.from_iterable(genres2))
+
+     for i in genres1:
+          for y in genres2:
+               tot = tot + model.wv.similarity(i,y)
+               index+=1
+     
+     return tot/index
+
+
+model = model_genre()
