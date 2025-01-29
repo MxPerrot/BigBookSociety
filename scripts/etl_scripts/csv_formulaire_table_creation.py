938aa663a20b23d60b94be6e0cb4386314aeec9a
Spectre017 <spectre2162@gmail.com>
2025-01-29 15:11:55 +0100

    Restored older version of file

--- scripts/etl_scripts/csv_formulaire_table_creation.py
+++ scripts/etl_scripts/csv_formulaire_table_creation.py
@@ -87,7 +87,7 @@ def main(results):
     livre_df = pd.DataFrame({'titre': list(livres_preferes_unique)})
     livre_df.index = livre_df.index+max_id_livre1+1
     livre_df = livre_df.reset_index(names=['id_livre'])
-    livre_df = pd.merge(livre1_df, livre_df, how='outer', on='titre')
+    livre_df = pd.concat([livre1_df, livre_df])
     
     livre_df['nb_notes'] = livre_df['nb_notes'].astype('Int64') # force convert numerical values to int
     livre_df['nb_critiques'] = livre_df['nb_critiques'].astype('Int64') # force convert numerical values to int
@@ -99,29 +99,17 @@ def main(results):
     livre_df['nombre_pages'] = livre_df['nombre_pages'].astype('Int64') # force convert numerical values to int
     livre_df['isbn13'] = livre_df['isbn13'].astype('Int64') # force convert numerical values to int
     livre_df['id_editeur'] = livre_df['id_editeur'].astype('Int64') # force convert numerical values to int
-    livre_df['titre'] = livre_df['titre'].str.strip()
-    livre_df = livre_df.drop_duplicates('titre')
+
     livre_df.to_csv(os.path.join(PATH_POPULATE,"livre.csv"), index=False)
 
     # _auteur : id_auteur, nom
     # Fusion avec le csv déjà existant
     max_id_auteur1 = auteur1_df['id_auteur'].max()
     auteur_df = pd.DataFrame({'nom': list(auteurs_preferes_unique)})
+    auteur_df.index = auteur_df.index+max_id_auteur1+1
+    auteur_df = auteur_df.reset_index(names=['id_auteur'])
+    auteur_df = pd.concat([auteur1_df, auteur_df])
     
-
-    auteur_df['id_auteur'] = auteur_df.index+max_id_auteur1+1
-    # auteur_df = auteur_df.reset_index(names=['id_auteur'])
-
-
-    auteur_df = pd.merge(auteur_df, auteur1_df, how='outer', on='id_auteur')
-    for i in auteur_df:
-        if i["nom_x"] != NaN and i[nom_y] != NaN:
-            print(i)
-
-    auteur_df = auteur_df.drop_duplicates('nom')
-
-    auteur_df = auteur_df.sort_values(by=['id_auteur'])
-
     auteur_df['nb_critiques'] = auteur_df['nb_critiques'].astype('Int64') # force convert numerical values to int
     auteur_df['nb_reviews'] = auteur_df['nb_reviews'].astype('Int64') # force convert numerical values to int
 
