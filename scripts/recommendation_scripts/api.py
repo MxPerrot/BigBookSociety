b72b4b56db717905be9a79114c13f3c0a6892462
Spectre017 <spectre2162@gmail.com>
2025-01-29 11:12:57 +0100

    virgule

--- scripts/recommendation_scripts/api.py
+++ scripts/recommendation_scripts/api.py
@@ -7,25 +7,27 @@ from typing import Annotated
 
 modelGenres = ru.model_genre()
 cursor = bdd.setUpCursor()
-
-
 app = FastAPI()
 
 #https://fastapi.tiangolo.com/tutorial/first-steps/
 
 
-# parameters = q=foo&q=bar
-
 #q1 = user, q2 = nbrecommendation, q3 = limite
 @app.get("/get_book_item_based/")
-async def read_items(q: Annotated[list[str] | None, Query()] = None):
+async def get_book_item_based(q: Annotated[list[str] | None, Query()] = None):
     return recommendationItemBased(cursor, modelGenres, int(q[0]), int(q[1]), bdd.getLivresAEvaluer(cursor, int(q[2])))
 
 @app.get("/get_book_item_based_tendance/")
-async def read_items(q: Annotated[list[str] | None, Query()] = None):
+async def get_book_item_based_tendance(q: Annotated[list[str] | None, Query()] = None):
     return recommendationItemBased(cursor, modelGenres, int(q[0]), int(q[1]), bdd.getLivresAEvaluerTendance(cursor, int(q[2])))
 
 @app.get("/get_book_item_based_decouverte/")
-async def read_items(q: Annotated[list[str] | None, Query()] = None):
+async def get_book_item_based_decouverte(q: Annotated[list[str] | None, Query()] = None):
     return recommendationItemBased(cursor, modelGenres, int(q[0]), int(q[1]), bdd.getLivresAEvaluerDecouverte(cursor, int(q[2])))
 
+
+
+@app.get("/get_book_by_id/{id}")
+async def get_book_by_id(id):
+
+    return recommendationItemBased(cursor, modelGenres, int(q[0]), int(q[1]), bdd.getLivresAEvaluerDecouverte(cursor, int(q[2])))
