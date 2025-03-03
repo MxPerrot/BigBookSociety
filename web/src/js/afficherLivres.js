d35a8dfe38f9c43a835e1126451d9ef48728b108
MxPerrot <maxime.perrot@etudiant.univ-rennes1.fr>
2025-03-03 02:57:44 +0100

    fix: missing image now shows title

--- web/src/js/afficherLivres.js
+++ web/src/js/afficherLivres.js
@@ -4,30 +4,44 @@ function afficherLivres(livres, conteneur) {
     // D'abord, vider le conteneur
     conteneur.empty()
 
-    // Itération sur le tableau des livres
-    $.each(livres, function(index, livre) {
-        // Création d'une div pour chaque livre avec un id unique et une classe
-        var $livreItem = $('<div>', {
-            id: 'book' + (index + 1),
-            class: 'book-list-item'
-        });
+    // // Convertir livres en array si ce n'est pas fait
 
-        // Construction de l'URL de l'image en utilisant la propriété isbn13
-        
-        var urlImage = 'https://covers.openlibrary.org/b/isbn/' + livre.isbn13 + '-L.jpg?default=false';
-        // Création d'un élément img avec l'attribut src défini sur l'URL de l'image
-        
-        var $img = $('<img>', {
-            src: urlImage,
-            alt: livre.titre
+    // livres = JSON.parse(livres)
+
+    // Ensuite, vérifier qu'il y a bien des livres à afficher
+    if (livres.length > 0) {
+
+        // Itération sur le tableau des livres
+        $.each(livres, function(index, livre) {
+            // Création d'une div pour chaque livre avec un id unique et une classe
+            var $livreItem = $('<div>', {
+                id: 'book' + (index + 1),
+                class: 'book-list-item'
+            });
+
+            // Construction de l'URL de l'image en utilisant la propriété isbn13
+            
+            var urlImage = 'https://covers.openlibrary.org/b/isbn/' + livre.isbn13 + '-L.jpg?default=false';
+            // Création d'un élément img avec l'attribut src défini sur l'URL de l'image
+            
+            var $img = $('<img>', {
+                src: urlImage,
+                alt: livre.titre
+            });
+            
+            // Ajout de l'image à la div du livre
+            $livreItem.append($img);
+            
+            // Ajout de l'élément livre au conteneur
+            conteneur.append($livreItem);
+
         });
         
-        // Ajout de l'image à la div du livre
-        $livreItem.append($img);
-        
-        // Ajout de l'élément livre au conteneur
-        conteneur.append($livreItem);
-        console.log(conteneur.html())
+    } else {
 
-    });
+        conteneur.append("Pas de livres.")
+
+    }
+
+
 }
