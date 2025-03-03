01d7880ec855ace3bd2eaa4ad1597d7eda7ac93a
Spectre017 <spectre2162@gmail.com>
2025-03-03 15:27:00 +0100

    unignore

--- web/src/js/exempleAPI.js
+++ web/src/js/exempleAPI.js
@@ -1,13 +1,6 @@
-// Pour damien <3
-
-const url = "http://127.0.0.1:8000/users/me"
-async function getMyData(url) {
-    const result = await fetch(url);
-    console.log(result.json())
-}
+// Pour damien <3 
 
 
-getMyData(url)
 
 // <script src="exempleAPI.js" defer></script>
 // UTILISER JQUERY POUR ACCEDER AUX ELEMENTS HTML avec $("#id-de-l-element")...
