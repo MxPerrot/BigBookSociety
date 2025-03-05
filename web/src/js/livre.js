1bea2a5885e4353175e5ce4fa83458366b8a1d83
FlorianNormand <flo1.normand@gmail.com>
2025-03-05 13:37:00 +0100

    fix bug editeur et date publication

--- web/src/js/livre.js
+++ web/src/js/livre.js
@@ -15,7 +15,7 @@ function fetchBookById(id) {
     fetch(url)
         .then(response => response.json())
         .then(book => {
-            console.log("Données récupérées :", book[0].titre);
+            console.log("Données récupérées :", book);
             
             if (book) {
 
@@ -29,11 +29,11 @@ function fetchBookById(id) {
                     <img class="card-img" src="${coverUrl}" alt="Couverture du livre ${book[0].titre}" onerror="this.onerror=null;this.src='../../public/img/couverture.jpg';" />                </div>
                     <div class="card-content">
                         <h2 class="card-title"> ${book[0].titre || "Titre non disponible" }</h2>
-                        <h3 class="card-author"> De ${book[0].nom_auteur || "Auteur inconnu"}</h3>
-                        <p class="card-description"> Description: ${book[0].description || "Description non disponible"}</p>
-                        <h4 class="card-editeur">Edition: ${book[0].editeur || "Éditeur inconnu"}</h4>
+                        <h3 class="card-author"> ${book[0].nom_auteur || "Auteur inconnu"}</h3>
+                        <p class="card-description"> ${book[0].description || "Description non disponible"}</p>
+                        <h4 class="card-editeur">Edition: ${book[0].nom_editeur || "Éditeur inconnu"}</h4>
                         <h4 class="card-pages">Pages: ${book[0].nombre_pages || "Nombre de pages inconnu"}</h4>
-                        <h4 class="card-datesortie">Date de sortie: ${book[0].date_sortie || "Non disponible"}</h4>
+                        <h4 class="card-datesortie">Date de sortie: ${book[0].date_publication || "Non disponible"}</h4>
                     </div>
                 `;//<button class="bouton-ajouter">Ajouter livre</button>
             } else {
