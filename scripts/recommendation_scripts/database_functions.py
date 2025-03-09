941caa27984d1c40771a514377d3b67025b4bd66
MxPerrot <maxime.perrot@etudiant.univ-rennes1.fr>
2025-03-09 22:14:15 +0100

    feat: add auto retry to DB connexion
    This is so that when running the dockers, the api will wait for the
    database to have no transaction pending

--- scripts/recommendation_scripts/database_functions.py
+++ scripts/recommendation_scripts/database_functions.py
@@ -1,26 +1,40 @@
 import pandas as pd
+import time
 import os
 from dotenv import load_dotenv, dotenv_values 
 import psycopg2
+from psycopg2 import OperationalError
 import random as rd
 
-def setUpConnection():
+def setUpConnection(retries=5, delay=5):
     """
     Connects to the Database
     """
     # loading variables from .env file
     load_dotenv() 
 
-    connection = psycopg2.connect(
-        database=os.getenv("DATABASE_NAME"), 
-        user=os.getenv("USERNAME"), 
-        password=os.getenv("PASSWORD"), 
-        host=os.getenv("HOST"), 
-        port=os.getenv("PORT")
-    )
+    database=os.getenv("DATABASE_NAME")
+    user=os.getenv("DB_USERNAME") # WARNING: CANNOT BE USERNAME AS IT CONFLICTS WITH DEFAULT ENV VARIABLE
+    password=os.getenv("PASSWORD")
+    host=os.getenv("HOST")
+    port=os.getenv("PORT")
 
-    return connection
+    for i in range(retries):
+        try:
+            connection = psycopg2.connect(
+                database=database,
+                user=user,
+                password=password,
+                host=host,
+                port=port
+            )
+            print("Connected to DB!")
+            return connection
+        except OperationalError as e:
+            print(f"DB connection failed, retrying in {delay} seconds...")
+            time.sleep(delay)
 
+    raise Exception("Could not connect to the database after several retries.")
 
 def setUpCursor(connection):
     """
