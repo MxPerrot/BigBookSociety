b06bafeb00717308363c42deadf8854668bea67c
Asaiah-cosson <asaiah.cosson@etudiant.univ-rennes.fr>
2025-03-18 17:49:51 +0100

    Maj notations + like + lu + mini design header

--- web/src/js/header.js
+++ web/src/js/header.js
@@ -127,7 +127,7 @@ headerTemplate.innerHTML = `
 
         #BigBook{
             font-family: 'AbrilFatface';
-            font-size: 4.5rem;
+            font-size: 4rem;
         }
     </style>
 
